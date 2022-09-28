63ed66294add7d19977bbc4542ea5eacd4fd604a
Joshtdale <joshua.dale@student.awesomeinc.org>
2022-09-28 18:39:39 +0000

    my lord and savior josh

--- sandbox.js
+++ sandbox.js
@@ -1,2 +1,34 @@
+// const { default: axios } = require("axios")
+
+const apiKey = '7e8e7a9139e4f09fb253db0d8ddce620'
 const mainContainer = document.getElementById('main_container')
 
+let weatherConditions = [
+    {
+        city: '',
+        temp: '',
+
+    }
+]
+
+async function getWeather() {
+    const response = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?zip=40517,us&appid=${apiKey}`)
+    // initializeContent(response.data.list[0])
+    console.log(response.data)
+};
+getWeather()
+
+parent = mainContainer
+function createLayout(parentEl, tag, text) {
+    let element = document.createElement(tag)
+    element.textContent = text
+    parentEl.appendChild(element)
+}
+
+function initializeContent() {
+    createLayout(mainContainer, 'div', 'value text') 
+
+
+};
+
+initializeContent()
