f170146e46768ab99fc525b275dc4d71e2df1c27
Joshtdale <joshua.dale@student.awesomeinc.org>
2022-09-29 14:39:57 +0000

    data passing

--- sandbox.js
+++ sandbox.js
@@ -1,4 +1,5 @@
 const apiKey = '7e8e7a9139e4f09fb253db0d8ddce620';
+const zip = '40517'
 
 let weatherConditions = [
     {
@@ -39,10 +40,10 @@ let day4 = weatherConditions[3]
 
 
 async function getWeather() {
-    const response = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?zip=40517,us&appid=${apiKey}`)
-    let data = response.data;
-    initializeContent(data)
-    // console.log(response.data.city.name)
+    const response = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?zip=${zip},us&appid=${apiKey}`)
+    // data = response.data;
+    initializeContent(response.data)
+    // console.log(response.data.city.name)       
     // initializeContent(response.data)
     // day1.city = response.data.city.name
 };
@@ -51,7 +52,7 @@ getWeather()
 //parent = mainContainer
 function createLayout(parentEl, tag, text, className, idName,) {
     let element = document.createElement(tag)
-    element.textContent = text
+    element.innerText = text
     if (className) {
         element.setAttribute('class', className)
     };
@@ -64,7 +65,7 @@ function createLayout(parentEl, tag, text, className, idName,) {
 function initializeContent(data) {
     const mainContainer = document.getElementById('main_container')
     //nope
-    console.log(day1.city)
+    // console.log(day1.city)
 
     //header
     createLayout(mainContainer, 'div', '', 'col card', 'headContainer')
@@ -80,7 +81,7 @@ function initializeContent(data) {
     // city
     createLayout(mainContainer, 'div', '', 'row', 'cityContainer')
     createLayout(cityContainer, 'div', '', 'col', 'cityCol')
-    createLayout(cityCol, 'div', 'City', 'text-center',)
+    createLayout(cityCol, 'div', day1.city, 'text-center',)
 
     // Temp
     createLayout(mainContainer, 'div', 'Temp')
@@ -93,10 +94,10 @@ function initializeContent(data) {
     createLayout(mainContainer, 'div', 'stuff')
 
     // console.log(getWeatherBtn)
-
     day1.city = data.city.name
     console.log(day1.city)
+    // console.log(data)
 };
 
 
-initializeContent()
+// initializeContent()
